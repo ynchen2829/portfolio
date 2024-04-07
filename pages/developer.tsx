13ac63f4f89eaefbbca95bc3b05802232f7cc41f
ynchen2829 <ynchen2829@gmail.com>
2024-04-06 20:45:26 -0700

    removed download resume

--- pages/developer.tsx
+++ pages/developer.tsx
@@ -38,7 +38,7 @@ function developer(){
                             In short, I make websites: front to back, UI to database. I also like exploring cloud technologies and distributed systems as an 
                             extension from my college interests.
                         </p>
-                        <Link href={'/Yinuo Chen Resume.pdf'} target="_blank"
+                        {/* <Link href={'/Yinuo Chen Resume.pdf'} target="_blank"
                         className="border focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center focus:ring-gray-600 bg-gray-800 border-gray-700 text-white hover:bg-gray-700 mr-2 mb-2">
                             <svg aria-hidden="true" className="w-6 h-5 mr-2 -ml-1" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff">
                                 <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
@@ -48,7 +48,7 @@ function developer(){
                                 </g>
                             </svg>
                             Download Resume
-                        </Link>
+                        </Link> */}
                     </div>
                     <div className="md:col-span-1 md:order-1 px-4 md:ml-20 flex justify-center md:justify-start">
                         <div>
